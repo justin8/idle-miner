2e336f2b0b7107b1842600b32a4b777f066528b5
Justin Dray <justin@dray.be>
2021-07-03 11:57:27 +1000

    Change to ethminer

--- README.md
+++ README.md
@@ -8,9 +8,20 @@ Idle detection mechanisms:
 
 Cryptocurrencies supported:
 
-- ETH (PhoenixMiner)
+- ETH (ethminer)
 
 ## Requirements
 
-- PhoenixMiner (Have it available in the path)
-  - CUDA/OpenCL
+- CUDA/OpenCL (For CUDA: `apt-get install nvidia-cuda-toolkit` on Debian derivatives)
+
+## Usage
+
+`./install.sh $WALLET_ADDRESS`
+
+That is all, now your system will mine to that address while your lock screen is active.
+
+The control process logs can be viewed with: `journalctl --user -u mine-while-idle`
+
+The miner logs can be viewed with: `less /tmp/miner-log`
+
+**NOTE:** You can turn on alerts from the ethermine dashboard for when your system goes offline
--- install.sh
+++ install.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 set -eo pipefail
 
+# Note: Deleting /usr/local/bin/ethminer will result in an automatic update of the miner installation
+
 verify_prerequisites() {
-  if ! which PhoenixMiner &>/dev/null; then
-    echo "Cannot find PhoenixMiner in \$PATH. Please install it first"
-    exit 1
+  if ! command -v ethminer &>/dev/null; then
+    echo "Cannot find ethminer. Downloading..."
+    (
+      sudo mkdir -p /opt/ethminer
+      cd /opt/ethminer
+      sudo curl -Lo ethminer.tar.gz "$(curl -s https://api.github.com/repos/ethereum-mining/ethminer/releases/latest |
+        grep "browser_download_url.*cuda-9-linux.*" |
+        cut -d '"' -f 4)"
+      sudo tar xf ethminer.tar.gz
+      sudo rm -f ethminer.tar.gz
+      sudo ln -sf /opt/ethminer/bin/ethminer /usr/local/bin/ethminer
+      echo "Successfully installed ethminer"
+    )
   fi
 
-  if ! which screen &>/dev/null; then
-    echo "Cannot find screen in \$PATH. Please install it first"
-    exit 1
+  if ! command -v screen &>/dev/null; then
+    echo "Cannot find screen in \$PATH. Installing..."
+    sudo apt-get install -y screen
   fi
 
 }
-
 
 SERVICE_NAME="mine-while-idle"
 SERVICE_FILE_PATH="$HOME/.local/share/systemd/user/$SERVICE_NAME.service"
@@ -22,6 +33,7 @@ WALLET_ADDRESS=$1
 cd "$(dirname "$(readlink -f "$0")")"
 
 if [[ $1 == "uninstall" ]]; then
+  echo "Uninstalling..."
   systemctl --user disable --now $SERVICE_NAME
   rm "$SERVICE_FILE_PATH"
   exit 0
@@ -40,9 +52,12 @@ set -u
 
 verify_prerequisites
 
+echo "Installing..."
 mkdir -p "$HOME/.local/bin"
 cp mine-while-idle "$HOME/.local/bin/mine-while-idle"
 
 mkdir -p "$(dirname "$SERVICE_FILE_PATH")"
-sed "s/WALLET_ADDRESS/$WALLET_ADDRESS/" service-template > "$SERVICE_FILE_PATH"
-systemctl --user enable --now $SERVICE_NAME
+sed "s/WALLET_ADDRESS/$WALLET_ADDRESS/" service-template >"$SERVICE_FILE_PATH"
+systemctl --user enable $SERVICE_NAME
+systemctl --user restart $SERVICE_NAME
+echo "Successfully installed"
